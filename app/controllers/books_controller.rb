4810ea23dcf772ae99b5560c8234d40b254940f6
EC2 Default User <ec2-user@ip-172-31-38-117.ec2.internal>
2022-06-02 08:08:46 +0000

    add

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,58 @@
+class BooksController < ApplicationController
+  def new
+    @book_new = Book.new
+  end
+
+  def index
+    @book_new = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    if @book_new.save
+      redirect_to book_path(@book_new), notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_comment = BookComment.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "homes#top"
+  get "home/about"=>"homes#about"
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  get "search" => "searches#search"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
